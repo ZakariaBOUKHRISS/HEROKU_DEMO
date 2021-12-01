ff2fcc8b86f9599587b8e9e888b2de955e975a11
ZakariaBOUKHRISS <zakaria.boukhriss@ump.ac.ma>
2021-12-01 01:26:42 +0100

    ports change

--- Demo.py
+++ Demo.py
@@ -18,3 +18,9 @@ if __name__ == "__main__":
     app.secret_key = 'ItIsASecret'
     app.debug = True
     app.run()
+
+    # ----Remainder of Code----
+
+if __name__ == '__main__':
+  port = int(os.environ.get('PORT', 5000))
+  app.run(host = '0.0.0.0', port = port)
